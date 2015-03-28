0176d3c9e004b96c335b4bca9d6f8b0e1b6e0f42
Yakir <yakirgoaron@gmail.com>
2015-03-28 13:14:47 +0300

    add project

---
+++ MMSServerLocal/src/com/server/MMSServer/MMSServerLocalServlet.java
@@ -0,0 +1,14 @@
+package com.server.MMSServer;
+
+import java.io.IOException;
+import javax.servlet.http.*;
+
+@SuppressWarnings("serial")
+public class MMSServerLocalServlet extends HttpServlet {
+	public void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws IOException {
+		//resp.setContentType("text/plain");
+		//resp.getWriter().println("Hello, world");
+		resp.sendRedirect("MMSPage.php");
+	}
+}
