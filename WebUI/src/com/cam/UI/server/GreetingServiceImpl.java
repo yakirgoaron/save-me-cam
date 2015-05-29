5d0d0e70b7e72cf04a18f5aacdc0631384eab1a9
Yakir <yakirgoaron@gmail.com>
2015-05-29 12:36:07 +0300

    asdasd

--- WebUI/src/com/cam/UI/server/GreetingServiceImpl.java
+++ WebUI/src/com/cam/UI/server/GreetingServiceImpl.java
@@ -1,17 +1,24 @@
 package com.cam.UI.server;
 
+import java.io.IOException;
+import java.util.logging.Logger;
+
 import com.cam.UI.client.GreetingService;
 import com.cam.UI.shared.FieldVerifier;
+import com.google.gwt.dom.builder.shared.QuoteBuilder;
 import com.google.gwt.user.server.rpc.RemoteServiceServlet;
-
+import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
+import com.google.api.client.json.gson.GsonFactory;
 /**
  * The server-side implementation of the RPC service.
  */
 @SuppressWarnings("serial")
 public class GreetingServiceImpl extends RemoteServiceServlet implements
 		GreetingService {
-
+	private static final Logger logger = Logger.getLogger("GreetingServiceImpl");
 	public String greetServer(String input) throws IllegalArgumentException {
+		String a = "Fail";
+		logger.fine("start 1");
 		// Verify that the input is valid. 
 		if (!FieldVerifier.isValidName(input)) {
 			// If the input is not valid, throw an IllegalArgumentException back to
@@ -26,9 +33,27 @@ public class GreetingServiceImpl extends RemoteServiceServlet implements
 		// Escape data from the client to avoid cross-site script vulnerabilities.
 		input = escapeHtml(input);
 		userAgent = escapeHtml(userAgent);
-
-		return "Hello, " + input + "!<br><br>I am running " + serverInfo
-				+ ".<br><br>It looks like you are using:<br>" + userAgent;
+		logger.fine("new 1");
+		Logincam.Builder builder = new Logincam.Builder(UrlFetchTransport.getDefaultInstance(), new GsonFactory(), null);
+		logger.fine("new 2");
+		Logincam service = builder.build();
+		logger.fine("new 3");
+		Logincam quote = new Logincam(builder);
+		logger.fine("new 4");
+		String b = "OK";
+		try {
+			logger.fine("new 5");
+			quote.login(input, "123").execute();
+			a = "SUCCESS";
+			logger.fine("new 6");
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			b = e.getMessage() + "<br>";
+			b += e.getLocalizedMessage();
+			a = "EXECPTION";
+		}
+		return "Hello, " + b + "!<br><br>I am running " + serverInfo
+				+ ".<br><br>It looks like -- "+a+" --  you are using:<br>" + userAgent;
 	}
 
 	/**
