8b167283a4f67aebc385747ab1bddc005442a3ea
Yakir <yakirgoaron@gmail.com>
2015-08-22 14:21:31 +0300

    2313

--- WebUI/src/com/cam/UI/shared/UsersForCameraLocal.java
+++ WebUI/src/com/cam/UI/shared/UsersForCameraLocal.java
@@ -7,5 +7,5 @@ package com.cam.UI.shared;
 public class UsersForCameraLocal  implements java.io.Serializable {
 	public String name;
 	public java.lang.String phone;
-	public java.lang.Double mail;
+	public java.lang.String mail;
 }
